7b893d9f5d548b4b33c2e768ab555145fe83954c
Emanuel De Jesús Tejada Coste <e.coste@sisalril.gov.do>
2022-01-19 10:45:12 -0400

    Express server configuration

--- server/index.js
+++ server/index.js
@@ -1,4 +1,25 @@
-console.log("Hi from node");
-console.log("Hi from node");
-console.log("Hi from node");
-console.log("Hi from node");
+const express = require("express");
+const app = express();
+
+
+
+
+app.get('/login', (req, res ) => {
+
+    console.log(req)
+
+    res.send({
+        ok:true,
+        msg:"everything ok",
+        user: "5a5ds42135d4sd"
+    });
+});
+
+
+
+const PORT = 5000 || process.env.PORT
+
+
+app.listen( PORT , () =>{
+    console.log(`Server listening por: ${PORT}`);
+})
