df89c7fe4a0f519cb65039fb826686ddbaf57aef
EmanuelTejada032GameDev <emanueltejada311295@gmail.com>
2022-01-26 22:40:26 -0400

    Add MongoAtlas cluster connection string, db config file

---
+++ server/db/config.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+
+
+const dbConnection = async() => {
+    try {
+        mongoose.connect(process.env.CONNECTION,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+        console.log("Database is up");
+    } catch (error) {
+        console.log(error);
+        throw new Error('Database error on start');
+    }
+}
+
+module.exports = {
+    dbConnection
+}
--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const { dbConnection } = require("./db/config");
+
 require("dotenv").config();
 
+
 const app = express();
+dbConnection();
 
 
-
-const authRoutes =  require('./routes/auth')
+const authRoutes =  require('./routes/auth');
 
 
 
