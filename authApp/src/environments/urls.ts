14db0f5c33d5d37f3130b207946e0bd34d660b9d
EmanuelTejada032 <emanueltejada31295@gmail.com>
2022-06-06 22:03:55 -0400

    Add prod url to urls file

--- authApp/src/environments/urls.ts
+++ authApp/src/environments/urls.ts
@@ -1,5 +1,6 @@
+import { environment } from './environment.prod';
 
-const hostPlusBaseUrl = 'http://localhost:5000/api/'
+const hostPlusBaseUrl = (environment.production)?'http://localhost:5000/api/' : 'https://simple-angular-node-authapp.herokuapp.com/api'
 export const urls = {
     auth:{
         login: hostPlusBaseUrl + 'auth/login',
